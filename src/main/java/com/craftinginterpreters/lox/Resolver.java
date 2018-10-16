78f20cce2a600083fb35b4e30d68b13495d2e9f2
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-10-16 14:08:20 -0300

    concluido 12.1

--- src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -33,6 +33,14 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     return null;                               
   }
 
+  @Override                                    
+  public Void visitClassStmt(Stmt.Class stmt) {
+    declare(stmt.name);
+
+    define(stmt.name);                         
+    return null;                               
+  }
+
   @Override                                              
   public Void visitExpressionStmt(Stmt.Expression stmt) {
     resolve(stmt.expression);                            
