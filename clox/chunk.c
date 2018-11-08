66600db421bab5ada5e4869eaa44131c1a2e0a5b
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-11-08 13:43:33 -0300

    concluida 14.6 (e 14)

--- clox/chunk.c
+++ clox/chunk.c
@@ -8,6 +8,7 @@ void initChunk(Chunk* chunk) {
   chunk->count = 0;           
   chunk->capacity = 0;        
   chunk->code = NULL;         
+  chunk->lines = NULL;
   initValueArray(&chunk->constants);
 }
 
@@ -17,15 +18,19 @@ void freeChunk(Chunk* chunk) {
   initChunk(chunk);                                 
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {     
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {     
   if (chunk->capacity < chunk->count + 1) {       
     int oldCapacity = chunk->capacity;            
     chunk->capacity = GROW_CAPACITY(oldCapacity); 
     chunk->code = GROW_ARRAY(chunk->code, uint8_t,
         oldCapacity, chunk->capacity);            
+    chunk->lines = GROW_ARRAY(chunk->lines, int,  
+        oldCapacity, chunk->capacity);            
+
   }
 
   chunk->code[chunk->count] = byte;               
+  chunk->lines[chunk->count] = line;
   chunk->count++;                                 
 }
 
