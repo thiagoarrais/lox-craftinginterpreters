fb49ed34b85b25c9b31b1ba5e74fb2f1b0ed8a52
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-11-13 14:07:39 -0300

    challenge 15.1

--- clox/main.c
+++ clox/main.c
@@ -9,22 +9,26 @@ int main(int argc, const char* argv[]) {
   Chunk chunk;                          
   initChunk(&chunk);                    
 
-  int constant = addConstant(&chunk, 1.2);
+  int constant = addConstant(&chunk, 1.0);
   writeChunk(&chunk, OP_CONSTANT, 123);        
   writeChunk(&chunk, constant, 123);
-  
-  constant = addConstant(&chunk, 3.4);    
+  constant = addConstant(&chunk, 2.0);    
   writeChunk(&chunk, OP_CONSTANT, 123);   
   writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);        
-
-  constant = addConstant(&chunk, 5.6);    
+  constant = addConstant(&chunk, 3.0);
   writeChunk(&chunk, OP_CONSTANT, 123);   
-  writeChunk(&chunk, constant, 123);      
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_ADD, 123);
+  constant = addConstant(&chunk, 4.0);
+  writeChunk(&chunk, OP_CONSTANT, 123);   
+  writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 5.0);
+  writeChunk(&chunk, OP_CONSTANT, 123);   
+  writeChunk(&chunk, constant, 123);
   writeChunk(&chunk, OP_NEGATE, 123);
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);       
 
