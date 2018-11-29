7b708d58b50b3dc0052161aaf9852f8ba452c496
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-11-29 13:06:55 -0300

    concluida 17.3

--- clox/compiler.c
+++ clox/compiler.c
@@ -14,6 +14,12 @@ typedef struct {
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {                          
+  return compilingChunk;                                
+}
+
 static void errorAt(Token* token, const char* message) {       
   if (parser.panicMode) return;
   parser.panicMode = true;
@@ -60,14 +66,37 @@ static void consume(TokenType type, const char* message) {
   errorAtCurrent(message);                                
 }
 
+static void emitByte(uint8_t byte) {                     
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);                                   
+  emitByte(byte2);                                   
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);    
+}
+
+static void endCompiler() {
+  emitReturn();            
+}
+
+void expression() { 
+  // What goes here?
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);            
 
+  compilingChunk = chunk;
   parser.hadError = false; 
   parser.panicMode = false;
 
   advance();                                      
   expression();                                   
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }                                 
