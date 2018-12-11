558854137052d64473b5356ca49653a55aa492c8
Thiago Arrais <thiago.souza@serpro.gov.br>
2018-12-11 13:35:26 -0300

    concluida 18.4.1

--- clox/compiler.c
+++ clox/compiler.c
@@ -179,6 +179,7 @@ static void unary() {
 
   // Emit the operator instruction.              
   switch (operatorType) {                        
+    case TOKEN_BANG: emitByte(OP_NOT); break;
     case TOKEN_MINUS: emitByte(OP_NEGATE); break;
     default:                                     
       return; // Unreachable.                    
@@ -197,7 +198,7 @@ ParseRule rules[] = {
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON       
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_SLASH           
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_STAR            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG            
+  { unary,    NULL,    PREC_NONE },       // TOKEN_BANG
   { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_BANG_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL           
   { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_EQUAL_EQUAL     
