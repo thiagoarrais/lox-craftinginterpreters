38f02a2ce51f02d21b40d2dacb495439f6848a4e
Thiago Arrais <thiago.souza@serpro.gov.br>
2019-06-07 13:23:14 -0300

    concluida 22.2

--- clox/compiler.c
+++ clox/compiler.c
@@ -157,6 +157,14 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void expression();                          
 static void statement();
 static void declaration();
@@ -331,6 +339,14 @@ void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
   uint8_t global = parseVariable("Expect variable name.");
 
@@ -395,6 +411,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
