ab95f4aff310afe7b03b4ae850eba8623955760b
Thiago Arrais <thiago.souza@serpro.gov.br>
2019-08-07 14:03:19 -0300

    concluida 23.4.2

--- clox/compiler.c
+++ clox/compiler.c
@@ -505,12 +505,26 @@ static void forStatement() {
 
   int loopStart = currentChunk()->count;
 
-  consume(TOKEN_SEMICOLON, "Expect ';'.");
+  int exitJump = -1;
+  if (!match(TOKEN_SEMICOLON)) {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+    // Jump out of the loop if the condition is false
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+  }
+
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
 
   statement();
 
   emitLoop(loopStart);
+
+  if (exitJump != -1) {
+    patchJump(exitJump);
+    emitByte(OP_POP); // Condition.
+  }
 
   endScope();
 }
