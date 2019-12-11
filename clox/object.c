efcb5b2c6f23ef98d4b69cf748f0040ac6730964
Thiago Arrais <thiago.souza@serpro.gov.br>
2019-12-11 13:52:06 -0300

    Concluida 25.3

--- clox/object.c
+++ clox/object.c
@@ -20,8 +20,14 @@ static Obj* allocateObject(size_t size, ObjType type) {
 }
 
 ObjClosure* newClosure(ObjFunction* function) {               
+  ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
+  for (int i = 0; i < function->upvalueCount; i++) {
+    upvalues[i] = NULL;
+  }
   ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
   closure->function = function;                               
+  closure->upvalues = upvalues;
+  closure->upvalueCount = function->upvalueCount;
   return closure;                                             
 }
 
@@ -89,6 +95,12 @@ ObjString* copyString(const char* chars, int length) {
   return allocateString(heapChars, length, hash);
 }
 
+ObjUpvalue* newUpvalue(Value* slot) {
+  ObjUpvalue* upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+  upvalue->location = slot;
+  return upvalue;
+}
+
 static void printFunction(ObjFunction* function) {
   if (function->name == NULL) {
     printf("<script>");
@@ -110,6 +122,9 @@ void printObject(Value value) {
       break;
     case OBJ_STRING:                  
       printf("%s", AS_CSTRING(value));
-      break;                          
+      break;
+    case OBJ_UPVALUE:
+      printf("upvalue");
+      break;
   }                                   
 }
--- clox/object.h
+++ clox/object.h
@@ -23,6 +23,7 @@ typedef enum {
   OBJ_FUNCTION,
   OBJ_NATIVE,
   OBJ_STRING,     
+  OBJ_UPVALUE
 } ObjType;
 
 struct sObj {        
@@ -52,9 +53,16 @@ struct sObjString {
   uint32_t hash;
 };
 
+typedef struct sUpvalue {
+  Obj obj;
+  Value* location;
+} ObjUpvalue;
+
 typedef struct {        
   Obj obj;              
   ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
 } ObjClosure; 
 
 ObjClosure* newClosure(ObjFunction* function);
@@ -62,7 +70,7 @@ ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
-
+ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
