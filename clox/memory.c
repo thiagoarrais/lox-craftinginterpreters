817caa6858325f065a49c4e7ab2e246f7de93e37
Thiago Arrais <thiago.souza@serpro.gov.br>
2020-01-22 13:29:38 -0300

    concluida 26.2

--- clox/memory.c
+++ clox/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>                                               
 
 #include "common.h"                                               
+#include "compiler.h"
 #include "memory.h"                                               
 #include "vm.h"
 
@@ -22,6 +23,41 @@ void* reallocate(void* previous, size_t oldSize, size_t newSize) {
   }                                                               
 
   return realloc(previous, newSize);                              
+}
+
+void markObject(Obj* object) {
+  if (object == NULL) return;
+#ifdef DEBUG_LOG_GC
+  printf("%p mark ", (void*)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+
+  object->isMarked = true;
+}
+
+void markValue(Value value) {
+  if (!IS_OBJ(value)) return;
+  markObject(AS_OBJ(value));
+}
+
+static void markRoots() {
+  for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+    markValue(*slot);
+  }
+
+  for (int i = 0; i < vm.frameCount; i++) {
+    markObject((Obj*)vm.frames[i].closure);
+  }
+
+  for (ObjUpvalue* upvalue = vm.openUpvalues;
+       upvalue != NULL;
+       upvalue = upvalue->next) {
+    markObject((Obj*)upvalue);
+  }
+
+  markTable(&vm.globals);
+  markCompilerRoots();
 }
 
 static void freeObject(Obj* object) {                     
@@ -65,6 +101,8 @@ void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
 #endif
+
+  markRoots();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
